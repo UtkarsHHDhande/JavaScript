76a61463d63ac5c85a4a89c511d74c065b9303c2
UtkarsHHDhande <utkarshdhande1432@gmail.com>
2024-06-19 09:56:53 +0530

    math and num

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,38 @@
+const score = 500
+//console.log(score);
+
+const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+const otherNumber = 23.843
+
+//console.log(otherNumber.toPrecision(2));
+
+const hundreds = 10000000
+//console.log(hundreds.toLocaleString('en-In'));
+
+// ++++++++++++++++++++++++++ Maths ++++++++++++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(4.5));
+// console.log(Math.ceil(4.5));
+// console.log(Math.floor(4.5));
+// console.log(Math.min(4,5,7,9,8));
+// console.log(Math.max(4,5,7,9,8));
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() *(max-min + 1)) + min ) // Note Formula
+
+
+
+
