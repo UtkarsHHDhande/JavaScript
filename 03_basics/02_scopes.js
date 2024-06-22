80aaf88c3d88311d7308b165787019a30f329477
UtkarsHHDhande <utkarshdhande1432@gmail.com>
2024-06-22 13:12:38 +0530

    scopes

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,17 @@
+// var c = 30
+
+ let a =300
+
+if(true){
+    let a = 10
+    const b = 20
+    console.log("inner :",a);
+    function addnum(){
+        
+    }
+   
+}
+
+console.log(a);
+//console.log(b);
+// console.log(c);
