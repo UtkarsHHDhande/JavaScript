51629eab0c407620effeab4d5162301999d74b40
UtkarsHHDhande <utkarshdhande1432@gmail.com>
2024-06-22 12:30:45 +0530

    functions and parameters

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,38 @@
+
+function sayMyName() {
+console.log("U");
+console.log("T");
+console.log("K");
+console.log("A");
+console.log("R");
+console.log("S");
+console.log("H");
+ }
+
+//  sayMyName()
+
+// function addTwoNumber(num1,num2){
+//   console.log(num1 +num2)
+// }
+
+function addTwoNumber(num1,num2){
+
+//    let result = num1 + num2
+//    return result
+return num1+num2
+  }
+
+const result = addTwoNumber(3,4)
+// console.log("Result: ", result);
+
+function loginUserMessage(username = "ud"){
+    if(!username){
+           console.log("please enter a username");
+           return
+    }
+    return `${username} just logged in`
+}
+
+// console.log(loginUserMessage("Utkarsh"))
+console.log(loginUserMessage())
+
