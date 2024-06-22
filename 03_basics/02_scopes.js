38eafad79b9ff63310d2e21a4e3cd4774bf5523c
UtkarsHHDhande <utkarshdhande1432@gmail.com>
2024-06-22 13:37:26 +0530

    Scopes

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -5,13 +5,51 @@
 if(true){
     let a = 10
     const b = 20
-    console.log("inner :",a);
+   // console.log("inner :",a);
     function addnum(){
-        
+
     }
    
 }
 
-console.log(a);
+// console.log(a);
 //console.log(b);
 // console.log(c);
+
+function one(){
+    const username = "utkarsh"
+
+    function two (){
+        const website = " youtube"
+     console.log(username);
+    }
+    // console.log(website);
+
+    two()
+}
+
+// one()
+
+if(true){
+    const username = "utkarsh"
+    if (username === "utkarsh") {
+        const website = " youtube"
+        // console.log(username + website);
+    }
+    // console.log(website);
+}
+// console.log(username);
+
+// +++++++++++++++++++++++++++++ interesting ++++++++++++++++++++++++++
+
+function addone(num){
+    return num + 1
+}
+
+console.log(addone(6))
+addtwo(6)
+const addtwo = function(num){
+    return num + 1
+}
+
+console.log(addtwo)
