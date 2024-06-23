0ac810ae6cff8592a879188ecdbce2cd46f84feb
UtkarsHHDhande <utkarshdhande1432@gmail.com>
2024-06-23 18:26:48 +0530

    iife

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,12 @@
+// Immediately Invoked Fuction Expressions (IIFE)
+
+
+(function chai(){
+    // named IIFE
+    console.log(`DB CONNECTED`);
+}) ();
+
+( (name) =>  {
+    console.log(`DB CONNECTED TWO ${name}`);
+} ) ('utkarsh')
+
