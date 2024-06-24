6da643c30e2ca0b0f03e032769246188a1ba9b73
UtkarsHHDhande <utkarshdhande1432@gmail.com>
2024-06-24 18:21:30 +0530

    switch case

---
+++ 04_control_flow/switch.js
@@ -0,0 +1,38 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 6
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+    case 5:
+        console.log("may");
+        break;
+    case 6:
+        console.log("june");
+        break;
+    case 7:
+        console.log("july");
+        break;
+
+    default:
+        console.log("default case match");
+        break;
+}
